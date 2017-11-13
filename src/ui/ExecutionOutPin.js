dc0696c0752890b1dd3bacd97ad8bc6ee7c3ed10
Liy <liy8272@gmail.com>
2017-11-13 23:56:58 +0000

    Wrong line style...

--- src/ui/ExecutionOutPin.js
+++ src/ui/ExecutionOutPin.js
@@ -32,18 +32,28 @@ export default class ExecutionOutPin extends ExecutionPin
     // let r = Math.atan2(dy, dx)
     // console.log(r/(Math.PI/180) )
     
-    let dx = this.connectedPin.position.x - this.position.x;
-    let dy = this.connectedPin.position.y - this.position.y;
+    let dx = (this.connectedPin.position.x-30) - (this.position.x+30);
+    let dy = (this.connectedPin.position.y) - (this.position.y);
     let adx = Math.abs(dx);
     let ady = Math.abs(dy);
 
-    // if(dx > 0) {
-      // if(ady < adx) {
-        this.path.setAttribute('d', `M${this.position.x},${this.position.y} l30,0 L${this.position.x + dx/2 - ady/2},${this.position.y} ${this.position.x + dx/2 + ady/2},${this.connectedPin.position.y} ${this.connectedPin.position.x-30},${this.connectedPin.position.y} ${this.connectedPin.position.x},${this.connectedPin.position.y}`);
-      // }
-      // else {
-        // this.path.setAttribute('d', `M${this.position.x},${this.position.y} L${this.position.x + dx/2},${this.position.y} ${this.position.x + dx/2},${this.connectedPin.position.y} ${this.connectedPin.position.x},${this.connectedPin.position.y}`);      
-      // }
+    console.log(dx, dy)
+
+    // if(dx < 0) {
+
+    // }
+    // else {
+      if(adx > ady) {
+        this.path.setAttribute('d', `M${this.position.x},${this.position.y} l30,0 ${dx/2-ady/2},0 ${ady},${dy} L${this.connectedPin.position.x-30},${this.connectedPin.position.y} ${this.connectedPin.position.x},${this.connectedPin.position.y}`);
+      }
+      else {
+        if(dy<0) {
+          this.path.setAttribute('d', `M${this.position.x},${this.position.y} l30,0 0,${dy/2+dx/2} ${dx},${-dx} L${this.connectedPin.position.x-30},${this.connectedPin.position.y} ${this.connectedPin.position.x},${this.connectedPin.position.y}`);
+        }
+        else {
+          this.path.setAttribute('d', `M${this.position.x},${this.position.y} l30,0 0,${dy/2-dx/2} ${dx},${dx} L${this.connectedPin.position.x-30},${this.connectedPin.position.y} ${this.connectedPin.position.x},${this.connectedPin.position.y}`);
+        }
+      }
     // }
   }
 }
