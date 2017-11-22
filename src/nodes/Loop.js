f19091098d7d7a391903964f5c8f79961dcc2055
Liy <liy8272@gmail.com>
2017-11-22 09:54:50 +0000

    Change Loop so it needs to be executed multiple times...
    until the limit is reached, then completed will run.
    
    This allows a user to put any task has "delay" behaviour in during the
    repeatitions. All they have to do is connect body route back to the start
    of loop task.
    
    Another "Repeat" task will be introduced to simulate the traditional "For
    Loop"

--- src/nodes/Loop.js
+++ src/nodes/Loop.js
@@ -10,17 +10,22 @@ export default class Loop extends Task
     this.execution.set('body');
     this.execution.set('completed');
 
-    this.inputs.addName('times');
+    this.inputs.addName('limit');
     this.outputs.addName('index');
+  }
+
+  prestart() {
+    this.index = 0;
   }
 
   run() {
     super.run()
-
-    for(let i=0; i<this.inputs.value('times'); ++i) {
-      this.outputs.assignValue('index', i);
+    this.outputs.assignValue('index', this.index);
+    if(++this.index > this.inputs.value('limit')) {
+      this.execution.run('completed')
+    }
+    else {
       this.execution.run('body')
     }
-    this.execution.run('completed')
   }
 }
