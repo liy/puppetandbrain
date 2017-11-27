2e76746e1781387215628673f23cd21c4c9eae71
Liy <liy8272@gmail.com>
2017-11-27 22:18:58 +0000

    Toggle boolean type...

--- src/graph/blocks/BranchBlock.js
+++ src/graph/blocks/BranchBlock.js
@@ -2,7 +2,16 @@ import TaskBlock from "./TaskBlock";
 
 export default class BranchBlock extends TaskBlock
 {
-  constructor(node, graph) {
-    super(node, graph);
+  constructor(node) {
+    super(node);
+
+    let pin = this.inputPins.get('condition');
+    let checkbox = pin.inputElement;
+    checkbox.value = node.inputs.value('condition');
+    checkbox.classList.add('boolean')
+
+    checkbox.addEventListener('click', e => {
+      checkbox.value = this.node.variables['condition'] = !this.node.variables['condition'];
+    })
   }
 }
