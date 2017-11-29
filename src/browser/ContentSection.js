ef6997222ab78666053c4bb5427b2926ad136dc7
Liy <liy8272@gmail.com>
2017-11-29 15:40:15 +0000

    Adding group

--- src/browser/ContentSection.js
+++ src/browser/ContentSection.js
@@ -1,29 +1,28 @@
 require('./ContentSection.scss')
 import DummyBlock from "./DummyBlock";
+import GroupName from './GroupName';
+import GroupGrid from './GroupGrid';
 
 export default class ContentSection
 {
   constructor() {
     this.element = document.createElement('div');
-    this.element.className = 'content-section';
+    this.element.className = 'browser-content-scroll-wrapper';
 
-    this.gridSection = document.createElement('div');
-    this.gridSection.className = 'grid-section';
-    // this.gridSection.style.overflowY = 'scroll';
-    this.element.appendChild(this.gridSection);
+    this.scroll = document.createElement('div');
+    this.scroll.className = 'browser-content-scroll';
+    this.element.appendChild(this.scroll);
 
-    this.blocks = [];
+    this.content = document.createElement('div');
+    this.content.className = 'browser-content';
+    this.scroll.appendChild(this.content);
 
-    for(let i=0; i<10; ++i) {
-      let block = new DummyBlock({
-        nodeName: 'test'
-      });
-      this.addBlock(block);
+    // 5 groups
+    for(let i=0; i<1; ++i) {
+      let groupName = new GroupName('group'+i);
+      this.content.appendChild(groupName.element);
+      let groupGrid = new GroupGrid();
+      this.content.appendChild(groupGrid.element);
     }
-  }
-
-  addBlock(block) {
-    this.gridSection.appendChild(block.gridBox);
-    this.blocks.push(block);
   }
 }
