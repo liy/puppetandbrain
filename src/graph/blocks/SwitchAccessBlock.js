d769aeecb42d46c4c34ec678a93a5299cc26c6af
Liy <liy8272@gmail.com>
2018-01-05 16:34:58 +0000

    Fix bar gadget

--- src/graph/blocks/SwitchAccessBlock.js
+++ src/graph/blocks/SwitchAccessBlock.js
@@ -13,9 +13,15 @@ export default class extends Block
     let debounceBar = new Bar();
     debounceBar.min = 0;
     debounceBar.max = 10;
-    debounceBar.decimalPlaces = 0;
+    debounceBar.decimalPlaces = 1;
 
-    this.inputPins.get('debounce').setGadget(new Bar());
-    this.inputPins.get('pre-acceptance').setGadget(new Bar());
+    
+    let preAcceptanceBar = new Bar();
+    preAcceptanceBar.min = 0;
+    preAcceptanceBar.max = 5;
+    preAcceptanceBar.decimalPlaces = 1;
+
+    this.inputPins.get('debounce').setGadget(debounceBar);
+    this.inputPins.get('pre-acceptance').setGadget(preAcceptanceBar);
   }
 }
--- src/graph/gadgets/Bar.js
+++ src/graph/gadgets/Bar.js
@@ -10,59 +10,88 @@ export default class Bar extends Gadget
 
     this.barSvg = new DOMParser().parseFromString(require('!raw-loader!../../assets/bar.svg'), "image/svg+xml").rootElement;
     this.barSvg.setAttribute('class', 'bar-svg');
-    this.barSvg.setAttribute('width', 70);
-    this.barSvg.setAttribute('height', 20);
     this.element.appendChild(this.barSvg);
 
     this.line = this.element.querySelector('#line');
 
-    this.input = document.createElement('input');
-    this.element.appendChild(this.input);
+    this.rangeSpan = document.createElement('span');
+    this.rangeSpan.className = 'range-span';
+    this.element.appendChild(this.rangeSpan);
 
     this.min = 0;
     this.max = 1;
 
     this.decimalPlaces = 2;
 
+    this.lastX = 0;
+
+    this.onDown = this.onDown.bind(this);
     this.onDrag = this.onDrag.bind(this);
+    this.onStop = this.onStop.bind(this);
   }
 
   init(node, name) {
     super.init(node, name)
-    
-    this.input.addEventListener('mousedown', e => {
-      e.preventDefault();
-    })
 
-    this.element.addEventListener('mousedown', e => {
-      e.stopPropagation();
-      document.addEventListener('mousemove', this.onDrag);
-    })
+    this.element.addEventListener('mousedown', this.onDown)
+    this.element.addEventListener('touchstart', this.onDown)
 
-    this.element.addEventListener('mouseup', e => {
-      document.removeEventListener('mousemove', this.onDrag);
-    })
+    document.addEventListener('mouseup', this.onStop)
+    document.addEventListener('touchend', this.onStop)
+
+    this.update();
+  }
+
+  destroy() {
+    this.element.removeEventListener('mousedown', this.onDown)
+    this.element.removeEventListener('touchstart', this.onDown)
+    document.removeEventListener('mousemove', this.onDrag);
+    document.removeEventListener('touchmove', this.onDrag);
+  }
+
+  onDown(e) {
+    e.stopPropagation();
+    document.addEventListener('mousemove', this.onDrag);
+    document.addEventListener('touchmove', this.onDrag);
+
+    let offsetX = e.offsetX ? e.offsetX : e.changedTouches[0].clientX-this.element.getBoundingClientRect().left
+    this.number = offsetX/80 * (this.min + (this.max-this.min));
+
+    this.lastX = e.clientX ? e.clientX : e.changedTouches[0].clientX;
   }
 
   onDrag(e) {
-    console.log(e)
+    let x = e.clientX ? e.clientX : e.changedTouches[0].clientX;
+    let inc = (this.max-this.min)/100;
+    let sign = Math.sign(x - this.lastX);
+    this.number += inc*sign;
+    this.lastX = x;
+  }
+
+  set number(n) {
+    n = Math.min(this.max, n);
+    n = Math.max(this.min, n);
+    this.node.variables[this.name] = n;
+    this.update();
+  }
+
+  get number() {
+    return this.node.variables[this.name];
   }
 
   onStop() {
-    
+    document.removeEventListener('mousemove', this.onDrag);
+    document.removeEventListener('touchmove', this.onDrag);
   }
 
-  get ratio() {
-    (this.max-this.min)
-  }
-
-  set ratio(v) {
-    this._ratio = Math.min(v, 1);
-    this.line.setAttribute('d', `M10 10h${50*this._ratio}`);
-    this.input.value = this.number.toFixed(this.decimalPlaces);
-  }
-
-  get value() {
-    return this.node.variables[this.name];
+  update() {
+    let ratio = Math.min(1, (this.number-this.min)/(this.max-this.min));
+    if(ratio == 0) {
+      this.line.setAttribute('d', '');
+    }
+    else {
+      this.line.setAttribute('d', `M10 10h${50*ratio}`);
+    }
+    this.rangeSpan.textContent = this.number.toFixed(this.decimalPlaces);
   }
 }
