618cb180931fb6393f11bb845c6bd71667ec8174
Liy <liy8272@gmail.com>
2018-01-12 14:45:51 +0000

    Template, POD  refactorin

--- src/NodeTemplate.js
+++ src/NodeTemplate.js
@@ -1,11 +1,11 @@
 window.NodeTemplate = Object.create(null);
 
 const test = {
-  
+
   ins: [{
     name: 'default',
 
-    // future changes: multiple execution in. Merge the callers field
+    // FUTURE: multiple execution in. Merge the callers field
     links: [{
       node: 123,
       name: 'name'
@@ -15,34 +15,42 @@ const test = {
     }]
   }],
 
+
   // TODO: rename to outs
   execution: [{
     // TODO: rename to name
     executionName: 'default',
 
-    // future changes
+    // FUTURE
     link: {
       node: 123,
       name: 'name'
     }
   }],
+
+
   inputs: [{
     // id: id of the input(pointer)
+
     // TODO: rename to name
     inputName: 'A',
     type: DataType.GENERIC,
+
     // TODO: add value property, which contains input default value.
 
-    // future changes: connection to a node
+    // TODO: rename output to link
     link: {
       node: 123,
       name: 'output name'
     }
   }],
+
+
   outputs: [{
     name: 'value',
     type: DataType.GENERIC,
 
+    // FUTURE: connection to a node
     links: [{
       node: 123,
       name: 'input name'
@@ -52,12 +60,14 @@ const test = {
     }]
   }],
 
+
   // TODO: rename to property, property of the node.
   // An authoring time variable for the node only.
   variables: {
     A: 123
   },
 
+  // visual related
   name: 'Test Block',
   elementClass: ['property', 'getter'],
   category: ['Action', 'Property']
