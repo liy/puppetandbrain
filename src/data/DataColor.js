e8d5c898847549a98c7c06475a7aa402bd3fb801
Liy <liy8272@gmail.com>
2018-01-15 21:22:24 +0000

    Refine data color...

--- src/data/DataColor.js
+++ src/data/DataColor.js
@@ -3,9 +3,9 @@ import DataType from "./DataType";
 export default {
   [DataType.GENERIC]: 0xA9C4D2,
   // [DataType.ACTOR]: 0xFFA6E0,
-  [DataType.ACTOR]: 0x94390d,
-  [DataType.COLOR]: 0x8C4AFF,
+  [DataType.ACTOR]: 0xC6A37D,
+  [DataType.COLOR]: 0xE884C9,
   [DataType.ARRAY]: 0xFF9900,
-  [DataType.VEC2]: 0xDE4048,
+  [DataType.VEC2]: 0xCC5050,
   [DataType.MAP]: 0x82C341
 }
