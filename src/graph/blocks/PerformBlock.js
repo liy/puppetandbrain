0eb97e1465454506910fa42a9d8068c8db8c7fbe
Liy <liy8272@gmail.com>
2018-01-16 11:38:59 +0000

    Fix action deleted first, then delete perform issue..

--- src/graph/blocks/PerformBlock.js
+++ src/graph/blocks/PerformBlock.js
@@ -17,15 +17,12 @@ export default class PerformBlock extends Block
   init(node) {
     super.init(node);
     
-    // TODO: probably not needed
-    if(this.node.action) {
-      this.node.action.outputs.on('output.added', this.onOutputAdded);
-    }
+    if(this.node.action) this.node.action.outputs.on('output.added', this.onOutputAdded);
   }
 
   destroy() {
     this.element.removeEventListener('mousedown', this.dbclick)
-    this.node.action.outputs.off('output.added', this.onOutputAdded);
+    if(this.node.action) this.node.action.outputs.off('output.added', this.onOutputAdded);
     super.destroy()
   }
 
