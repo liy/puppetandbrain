edd5dde5b10c796472132b0e3433409482e162e1
Liy <liy8272@gmail.com>
2018-01-31 17:17:56 +0000

    Fix the fetch options issue in Safari
    It does not like undeifined or default paramater in the constructor...

--- src/resources/Loader.js
+++ src/resources/Loader.js
@@ -3,7 +3,7 @@ import {Resource} from './Resource';
 
 export default class Loader extends EventEmitter
 {
-  constructor(id, url, options,) {
+  constructor(id, url, options) {
     super();
 
     this.id = id;
@@ -12,7 +12,7 @@ export default class Loader extends EventEmitter
   }
 
   start() {
-    return fetch(this.url, this.options).then(response => {
+    return fetch(this.url, this.options||{}).then(response => {
       if(response.ok) {
         // added to the resource
         return this.onSuccess(response).then(data => {
