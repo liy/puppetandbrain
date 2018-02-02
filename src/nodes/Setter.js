a5a7a57d34e01dd39112e19c104b9c869d0076a1
Liy <liy8272@gmail.com>
2018-02-02 14:49:36 +0000

    Should be type not data...

--- src/nodes/Setter.js
+++ src/nodes/Setter.js
@@ -32,7 +32,7 @@ export default class Setter extends Task
     // Note that I uses "input" for input id.
     // Originally it is set to be variableID but it can change when importing actor causing unnecessary complexity
     // SetterBlock will change the label using actual variable name.
-    this.inputs.add('input', this.variable.data);
+    this.inputs.add('input', this.variable.type);
     this.memory['input'] = this.memory['input'] || this.variable.data;
     // same rule as the input...
     this.outputs.assignProperty('output', {
