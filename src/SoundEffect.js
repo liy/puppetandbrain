b4797bfd0757df2461529c59356c56d613827168
Liy <liy8272@gmail.com>
2018-02-06 14:49:18 +0000

    Hack the sound effect preloading...

--- src/SoundEffect.js
+++ src/SoundEffect.js
@@ -1,20 +1,13 @@
-function loadSound(url) {
-  return fetch(url).then(async response => {
-    if(response.ok) {
-      let blob = await response.blob();
-      return URL.createObjectURL(blob);
-    }
-  })
-}
-
+// FIXME: find a better way to handle sound effect preloading and playing
 export default {
-  click: loadSound(require('./assets/sounds/click5.ogg')),
-  snap: loadSound(require('./assets/sounds/snap.mp3')),
-  link: loadSound(require('./assets/sounds/link.mp3')),
-  'banner-click': loadSound(require('./assets/sounds/switch27.ogg')),
+  click: new Audio(require(`./assets/sounds/click5.ogg`), require(`./assets/sounds/click5.mp3`)),
+  snap: new Audio(require(`./assets/sounds/snap.ogg`), require(`./assets/sounds/snap.mp3`)),
+  link: new Audio(require(`./assets/sounds/link.ogg`), require(`./assets/sounds/link.mp3`)),
+  'banner-click': new Audio(require(`./assets/sounds/switch27.ogg`), require(`./assets/sounds/switch27.mp3`)),
 
   play: async function(name) {
-    let audio = new Audio(await this[name])
+    let audio = this[name];
+    audio.currentTime = 0;
     audio.play().catch(err => {
       console.warn('Element removed causing sound effect gets unloaded?')
     })
