0fc0efb775c5eccaed6d00eb9264e3ba91181ecf
Liy <liy8272@gmail.com>
2018-02-07 18:03:38 +0000

    I've no idea why Audio does not auto pick mp3 if ogg not supported.
    For now, use mp3.

--- src/SoundEffect.js
+++ src/SoundEffect.js
@@ -1,10 +1,10 @@
 // FIXME: find a better way to handle sound effect preloading and playing
 export default {
-  click: new Audio(require(`./assets/sounds/click5.ogg`), require(`./assets/sounds/click5.mp3`)),
-  snap: new Audio(require(`./assets/sounds/snap.ogg`), require(`./assets/sounds/snap.mp3`)),
-  link: new Audio(require(`./assets/sounds/link.ogg`), require(`./assets/sounds/link.mp3`)),
-  'banner-click': new Audio(require(`./assets/sounds/switch27.ogg`), require(`./assets/sounds/switch27.mp3`)),
-  'trash': new Audio(require(`./assets/sounds/cardShove1.ogg`), require(`./assets/sounds/cardShove1.mp3`)),
+  click: new Audio(require(`./assets/sounds/click5.mp3`)),
+  snap: new Audio(require(`./assets/sounds/snap.mp3`)),
+  link: new Audio(require(`./assets/sounds/link.mp3`)),
+  'banner-click': new Audio(require(`./assets/sounds/switch27.mp3`)),
+  'trash': new Audio(require(`./assets/sounds/cardShove1.mp3`)),
 
   play: async function(name) {
     let audio = this[name];
