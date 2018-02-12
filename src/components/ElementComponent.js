edcb05f10e7d5ab24dace8a3351ba80d0f865cce
Liy <liy8272@gmail.com>
2018-02-12 17:41:00 +0000

    Fixed DOM and PIXI hybrid actor transformation issue.
    Just need to make sure the DOM overlay matches with canvas's size and
    position.
    Apply extra stage offset when deal with dom element mouse position.

--- src/components/ElementComponent.js
+++ src/components/ElementComponent.js
@@ -59,6 +59,8 @@ export default class ElementComponent extends Component
   }
 
   mouseDown(e) {
+    // note the stage offset applied, since clientX and clientY is in window coordinate.
+    // TODO: In theory, scale and rotation also needs to be applied. 
     this.entity.pointerDown(e.clientX - Editor.stage.offsetX, e.clientY - Editor.stage.offsetY, e);
   }
 
@@ -79,7 +81,7 @@ export default class ElementComponent extends Component
     
     this.matrix.rotate(this.rotation);
     this.matrix.scale(this.scale.x, this.scale.y);
-    this.matrix.translate(this.position.x + Editor.stage.offsetX, this.position.y + Editor.stage.offsetY);
+    this.matrix.translate(this.position.x, this.position.y);
     // centre the element to its origin
     this.matrix.translate(-this.element.offsetWidth/2, -this.element.offsetHeight/2)
 
