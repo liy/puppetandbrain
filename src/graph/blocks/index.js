e2cfdd8d38e297bffefd572a49c8be1f038014ff
Liy <liy8272@gmail.com>
2018-02-14 16:08:11 +0000

    Remove..

--- src/graph/blocks/index.js
+++ src/graph/blocks/index.js
@@ -1,10 +1,8 @@
 export Block from './Block';
 
 export ActionBlock from './ActionBlock';
-// export AnimationBlock from './AnimationBlock';
 export VariableGetterBlock from './VariableGetterBlock';
 export PerformBlock from './PerformBlock';
 export VariableSetterBlock from './VariableSetterBlock';
 // export ExtractorBlock from './ExtractorBlock';
-// export FlipBlock from './FlipBlock';
 export AdaptorBlock from './AdaptorBlock'
