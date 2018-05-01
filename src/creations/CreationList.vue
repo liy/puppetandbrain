7ce13a58a612f6ebe7a20a67538f852199a4f2f2
Liy <liy8272@gmail.com>
2018-05-01 12:24:30 +0100

    update

--- src/creations/CreationList.vue
+++ src/creations/CreationList.vue
@@ -29,27 +29,30 @@ export default {
 <style lang="scss" scoped>
 .creation-list {
   margin: auto;
-  // nav bar
-  padding: 60px 0;
+  // nav bar + 10px
+  padding: 65px 10px;
 }
 .activities-scroll {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  grid-row-gap: 2px;
-  grid-column-gap: 2px;
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
 }
 
 img {
   display: block;
   max-width: 100%;
   height: auto;
-  opacity: 0.5;
+  opacity: 0.8;
   transition: opacity 0.3s ease;
+
+  border-radius: 10px;
 
   cursor: pointer;
 }
 
 img:hover {
   opacity: 1;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.322);
 }
 </style>
